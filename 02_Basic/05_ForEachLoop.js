78d5a7ed08e40b32b3454729fbbdb28b4aa59d8c
admin07041999 <neeraj748026@gmail.com>
2024-12-07 07:36:40 +0530

    Foreach and Filter

---
+++ 02_Basic/05_ForEachLoop.js
@@ -0,0 +1,16 @@
+
+const programming = ['C#','Java','JavaScript','C','C++','Swift','Ruby','Python','Sql Server']
+
+const values = programming.forEach((item) =>{
+    //console.log(item); 
+    return item;
+})
+//console.log(values);
+
+programming.filter((item) => {
+    console.log(item);  
+    return true;  
+  });
+  
+
+
